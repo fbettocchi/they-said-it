c78a79d7a4a588b2326f1b88e9cbab76da6b1a1c
Fabrice Bettocchi <fabrice.bettocchi@gmail.com>
2022-02-26 16:05:29 +0100

    update config

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: 'eu-west-3_b0adeDhI1', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '7no47tf12lkm4gmf45d604s8qf', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'eu-west-3' // e.g. us-east-2
+        userPoolId: 'eu-west-3_b0adeDhI1',
+        userPoolClientId: '7no47tf12lkm4gmf45d604s8qf',
+        region: 'eu-west-3'
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
